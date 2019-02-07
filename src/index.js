18dd81a429ff8e64d78b265329979ea128f0f6a5
Håkon Solbjørg <hlsolbjorg@gmail.com>
2019-02-07 18:02:05 +0100

    feat: Add geopos route for getting geopositions for a list of public transport routes.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const fastify = require('fastify')({ logger: true });
 
+const paginate = require('./utils/pagination');
 const { getRealtime } = require('./handlers/et');
 const { getGeoPosition } = require('./handlers/geo');
 
@@ -38,6 +39,22 @@ fastify.get('/realtimetest', async (request, response) => {
         const data = await getRealtime(lineRef);
         response.type("application/json").code(200);
         return { data };
+    } catch (err) {
+        response.type("application/json").code(500);
+        return {
+            error: err.message,
+            message: "Something went wrong while parsing XML from Siri",
+        };
+    }
+});
+
+fastify.get('/geopos', async (request, response) => {
+    let { lineRef, num, skip } = request.query;
+
+    try {
+        data = await getGeoPosition(lineRef);
+        response.type("application/json").code(200);
+        return paginate(data, num, skip);
     } catch (err) {
         response.type("application/json").code(500);
         return {
